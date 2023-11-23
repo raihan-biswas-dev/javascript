010a937f8c3e3bc461d12505178e5e2d802e4acb
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-11-23 11:15:07 +0600

    Js basic

--- JavaScript/operator.js
+++ JavaScript/operator.js
@@ -1,3 +1,66 @@
-let a = 20;
-let b = 10;
-console.log(a + b);
+// console.log("Assignment Operator");
+
+// let a = 20;
+// let b = 10;
+// console.log(a + b);
+
+// console.log(a + "+" + b + "=" + (a + b)); // a + b = 30
+// console.log(a + "-" + b + "=" + (a - b)); // a - b = 10
+// console.log(a + "*" + b + "*" + a * b); // a * b = 200
+// console.log(a + "/" + b + "=" + a / b); // a / b = 2
+// console.log(a + "%" + b + "=" + (a % b)); // a % b = 0
+
+// let a = 20;
+
+//  a += 10; // a = a + 10;
+// a -= 10; // a = a - 10;
+// a *=10; // a = a * 10;
+// a /=10; // a = a / 10;
+//  a %=10; // a = a % 10;
+
+// console.log(a);
+
+// console.log("Conditional Operator");
+
+// let a = 300;
+// let b = 200;
+
+// let c = a < b ? 300 : 200;
+// console.log(c);
+
+// let myName = 'Raihan';
+
+// if(myName === 'Dola'){
+//     console.log("Yes I am dola")
+// }
+
+// let myAge = 10;
+
+// if (myAge > 19) {
+//   console.log("Dolar Biya korar boish hoiace");
+// }
+// if (myAge >= 20) {
+//   console.log("Dola is adult");
+// }
+// if (myAge < 20) {
+//   console.log("Dola is not Adult");
+// }
+
+// largest number in 3 number
+
+/*
+
+let a = 5;
+let b = 17;
+let c = 30;
+
+if(a > b && a > c){
+    console.log('A is large')
+}
+if(b > a && b > c){
+    console.log('B is large')
+}
+if(c > a && c > b){
+    console.log('C is large')
+}
+*/
---
+++ JavaScript/relational.js
@@ -0,0 +1,12 @@
+let m = 10;
+let n = 15;
+let o = 12;
+let p = 10;
+
+console.log(m + ">" + n + "=" + (m > n)); // m > n = false
+console.log(m + "<" + n + "=" + (m < n)); // m < n = true
+console.log(m + ">=" + o + "=" + (m >= o)); // m >= o = false
+console.log(m + "<=" + o + "=" + (m <= o)); // m <= o =  true
+console.log(m + "==" + o + "=" + (m == o)); // m == o <=  true
+console.log(m + "!=" + o + "=" + (m != o)); // m == o <=  true
+console.log(m + "==" + p + "=" + (m == p)); // m == o <=  true
