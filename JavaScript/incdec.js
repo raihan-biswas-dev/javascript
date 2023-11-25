94ff9e0eb8b7b387e43ae3b19015e70a2b93e483
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-11-25 14:44:41 +0600

    Logical Operations

---
+++ JavaScript/incdec.js
@@ -0,0 +1,4 @@
+let a = 10;
+
+console.log(--a)
+console.log(--a)
---
+++ JavaScript/logicalOperator.js
@@ -0,0 +1,20 @@
+// let a = 50;
+// let b = 100;
+
+// let  (a > 20) && (b <150)
+
+// console.log();
+
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("Dola Biya korte parbe");
+// } else {
+//   console.log("Biya korte parbe na");
+// }
+
+let bill = 500;
+
+if (bill >= 500) {
+    vat = bill * .15
+}
