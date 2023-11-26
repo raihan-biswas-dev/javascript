6d66cb004e097f299241a270f390a7e260a28277
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-11-26 13:15:46 +0600

    for loop

---
+++ JavaScript/loop.js
@@ -0,0 +1,24 @@
+// for (let i = 0; i <= 300; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 50; i = i * 2) {
+//   console.log(i);
+// }
+
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//   console.log("sum =", sum, "i=", i, "sum + i = ", sum + i);
+//   sum = sum + i;
+// }
+
+// console.log("sum = ", sum);
+
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  console.log("sum =", sum, "i=", i, "sum + i =", sum + i);
+  sum = sum + i;
+}
+
+console.log(sum);
--- JavaScript/switch.js
+++ JavaScript/switch.js
@@ -1,59 +1,75 @@
-let weekDay = "Thursday";
+// let weekDay = "Thursday";
 
-switch (weekDay) {
-  case "Saturday":
-    console.log("Saturday! Today is closed");
-    break;
-  case "Sunday":
-    console.log("Sunday! It is normal day");
-    break;
-  case "Monday":
-    console.log("Monday! it is normal day");
-    break;
-  case "Tuesday":
-    console.log("Tuesday! it is normal day");
-    break;
-  case "Wednesday":
-    console.log("wednesday! It is normal day");
-    break;
-  case "Thursday":
-    console.log("Thursday! It half working day day");
-    break;
-  case "Friday":
-    console.log("Friday! It is Off day");
-    break;
-  default:
-    console.log("Not a day");
-}
+// switch (weekDay) {
+//   case "Saturday":
+//     console.log("Saturday! Today is closed");
+//     break;
+//   case "Sunday":
+//     console.log("Sunday! It is normal day");
+//     break;
+//   case "Monday":
+//     console.log("Monday! it is normal day");
+//     break;
+//   case "Tuesday":
+//     console.log("Tuesday! it is normal day");
+//     break;
+//   case "Wednesday":
+//     console.log("wednesday! It is normal day");
+//     break;
+//   case "Thursday":
+//     console.log("Thursday! It half working day day");
+//     break;
+//   case "Friday":
+//     console.log("Friday! It is Off day");
+//     break;
+//   default:
+//     console.log("Not a day");
+// }
 
-const fruits = 4;
+// const fruits = 4;
 
-switch (fruits) {
-  case 0:
-    console.log("banana");
-    break;
-  case 1:
-    console.log("apple");
-    break;
-  case 2:
-    console.log("orange");
-    break;
-  case 3:
-    console.log("mango");
-    break;
-  default:
-    throw new Error("Invalid Number");
-}
+// switch (fruits) {
+//   case 0:
+//     console.log("banana");
+//     break;
+//   case 1:
+//     console.log("apple");
+//     break;
+//   case 2:
+//     console.log("orange");
+//     break;
+//   case 3:
+//     console.log("mango");
+//     break;
+//   default:
+//     throw new Error("Invalid Number");
+// }
 
-let theme = "dark";
-let bgColor = theme == "dark" ? "#333333" : "#efefef";
+// let theme = "dark";
+// let bgColor = theme == "dark" ? "#333333" : "#efefef";
 
-console.log(bgColor);
+// console.log(bgColor);
 
-if (theme == "dark") {
-  bgColor = "#333333";
-} else {
-  bgColor = "#efefef";
-}
+// if (theme == "dark") {
+//   bgColor = "#333333";
+// } else {
+//   bgColor = "#efefef";
+// }
+
+// console.log(bgColor);
+
+// let theme = "dark";
+// let bgColor = "";
+
+// if (theme == "light") {
+//   bgColor = "#f00";
+// } else {
+//   bgColor = "#efefef";
+// }
+
+// console.log(bgColor);
+
+let theme = "yellow";
+let bgColor = theme == "dark" ? "#efefef" : "blue";
 
 console.log(bgColor);
