3865b57309c19801696f1d00e678281adca093fe
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-11-26 14:05:24 +0600

    while

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -14,11 +14,20 @@
 
 // console.log("sum = ", sum);
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 1; i <= 10; i++) {
-  console.log("sum =", sum, "i=", i, "sum + i =", sum + i);
-  sum = sum + i;
+// for (let i = 1; i <= 10; i++) {
+//   console.log("sum =", sum, "i=", i, "sum + i =", sum + i);
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// ----------------While--------------
+
+let number = 1;
+
+while (number <= 10) {
+  console.log(number);
+  number++;
 }
-
-console.log(sum);
