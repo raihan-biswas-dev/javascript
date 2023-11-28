093fa8567e4196fc9de1dee97cbf0b992dbb6d06
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-11-28 23:52:24 +0600

    arr

--- JavaScript/arrar.js
+++ JavaScript/arrar.js
@@ -1,5 +1,52 @@
-const arr = [];
-arr[0] = 1;
-arr[1] = 2;
-arr[2] = 3;
-console.log(arr)
+/*
+
+let arr = new Array("Raihan", "Rashed", "Tareq", "Anik");
+console.log(arr);
+
+let arrName = ["jamal", "kamal", "Rahim", "faisal"];
+
+console.log(arrName);
+
+arrName[2];
+console.log(arrName.length); // Dot notation
+console.log(arrName["length"]); // Brackets Notation
+
+*/
+
+let arrName = ["Raihan", "Anik", "Kamal", "Jamal"];
+
+// for (i = 0; i < arrName.length; i++) {
+//   console.log("Name : ", arrName[i]);
+// }
+
+// arrName.push("Vutto");
+// arrName.pop();
+// arrName.shift();
+// arrName.unshift("I am new");
+// arrName.indexOf('Raihan')
+// console.log(arrName.indexOf('Kamal'));
+console.log(arrName.indexOf("Kamal"));
+
+// const arr = [];
+// arr[0] = 1;
+// arr[1] = 2;
+// arr[2] = 3;
+// arr[100] = 105;
+// console.log(arr);
+// console.log(arr.length);
+
+// Length = Last Index + 1;
+//Last Index = Length - 1;
+
+// let names = [
+//   "Raihan",
+//   "Rashed",
+//   "Hafiza",
+//   "habibur",
+//   "Asiya",
+//   "kushbu",
+//   "amar name",
+// ];
+// names.push('dola + Raihan')
+// names[names.length] = "Habib";
+// console.log(names);
--- JavaScript/tempCodeRunnerFile.js
+++ JavaScript/tempCodeRunnerFile.js
@@ -1,2 +1,2 @@
 
-}
+  console.log("Name : ", +arrName[i]);
