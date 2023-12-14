fcdc1bb7a6ef2c285cc3be050e17498e8a2cdfe9
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-12-14 14:43:02 +0600

    sum

--- JavaScript/arrar.js
+++ JavaScript/arrar.js
@@ -124,7 +124,7 @@ for (let i = 1; i < numbers.length; i++) {
 
 console.log(largestnumber);
 
-*/
+
 
 
 let response = new Array(9);
@@ -135,3 +135,14 @@ for(let i = 0; i < response.length; i++){
   response[i] = rand > 5 ? 'X' : 'O'
 }
 console.log(response)
+
+*/
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+console.log(sum);
