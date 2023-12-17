aba6dcb77481ab41c27ae898aa4167316faf9931
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-12-17 15:15:14 +0600

    One Dimentional Array traverse

---
+++ JavaScript/arrayOfArray.js
@@ -0,0 +1,12 @@
+let pointTable = [
+  [0, 0],
+  [3, 5],
+  [5, 8],
+  [1, 9],
+];
+
+//One Dimentional Array traverse
+
+for (let i = 0; i < pointTable.length; i++) {
+  console.log(`point ${i} x = ${pointTable[i][0]} y = ${pointTable[i][1]}`);
+}
--- JavaScript/arrayOffunction.js
+++ JavaScript/arrayOffunction.js
@@ -1,3 +1,4 @@
+/*
 let sum = (a, b) => a + b;
 let sub = (a, b) => a - b;
 let mul = (a, b) => a * b;
@@ -12,4 +13,22 @@ let b = 20;
 for (let i = 0; i < funcs.length; i++) {
   results = funcs[i](a, b);
   console.log(`${[funcs[i].name]} Results = ${results}`);
+}
+
+*/
+
+let sum = (a, b) => a + b;
+let sub = (a, b) => a - b;
+let mul = (a, b) => a * b;
+let div = (a, b) => a / b;
+let mod = (a, b) => a % b;
+
+let funcs = [sum, sub, mul, div, mod];
+
+let a = 10;
+let b = 5;
+
+for (let i = 0; i < funcs.length; i++) {
+  let results = funcs[i](a, b);
+  console.log(results);
 }
