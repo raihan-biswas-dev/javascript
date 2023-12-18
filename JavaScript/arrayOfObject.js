d5a6e0f05811e5eca023d6546be751dbecc6ceb4
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-12-18 07:38:02 +0600

    array of objects

---
+++ JavaScript/arrayOfObject.js
@@ -0,0 +1,32 @@
+// let p1 = {
+//   name: "Raihan",
+//   email: "raihan@gmail.com",
+// };
+// let p2 = {
+//   name: "Anik",
+//   email: "anik@gmail.com",
+// };
+// let p3 = {
+//   name: "alamin",
+//   email: "alamin@gmail.com",
+// };
+// console.log(people);
+
+let people = [
+  {
+    name: "Raihan",
+    email: "raihan@gmail.com",
+  },
+  {
+    name: "Anik",
+    email: "anik@gmail.com",
+  },
+  {
+    name: "alamin",
+    email: "alamin@gmail.com",
+  },
+];
+
+for (let p of people) {
+  console.log(`${p.name} ${p.email}`);
+}
