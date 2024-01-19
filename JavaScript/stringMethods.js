f62c6846baea211d195b59813e9b8615eb7f7731
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2024-01-19 18:32:46 +0600

    String Method

---
+++ JavaScript/stringMethods.js
@@ -0,0 +1,38 @@
+// let text = "JAVASCRIPT & REACT";
+// console.log(text.charAt(1));
+
+// JavaScript String at()
+
+// const name = "RAIHAN BISWAS";
+
+// The at() method returns the character at a specified index (position) in a string.
+
+// The at() method is supported in all modern browsers since March 2022:
+
+// console.log(name.at(2));
+// console.log(name[2]);
+
+// JavaScript String slice()==============
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(8, 13);
+// console.log(part);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7);
+// console.log(part);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12);
+
+// console.log(part);
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12, -6);
+// console.log(part);
+
+let str = "Apple, Banana, Kiwi";
+let part = str.substring(7, 13);
+console.log(part);
+
+
