edb63b12ae335fc58fe4c311c300c1c0041b697e
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2024-01-19 15:36:51 +0600

    string

---
+++ JavaScript/string.js
@@ -0,0 +1,13 @@
+// When using the == operator, x and y are equal:
+
+let x = "John";
+let y = new String("John");
+console.log(x == y);
+
+// When using the === operator, x and y are not equal:
+
+let a = "John";
+let b = new String("John");
+console.log(a === b);
+
+// Comparing two JavaScript objects always returns false.
