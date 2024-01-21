1161b5a06128153dd397be337c06092750bf48c2
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2024-01-21 12:39:08 +0600

    String Split

--- JavaScript/stringMethods.js
+++ JavaScript/stringMethods.js
@@ -31,8 +31,35 @@
 // let part = text.slice(-12, -6);
 // console.log(part);
 
-let str = "Apple, Banana, Kiwi";
-let part = str.substring(7, 13);
-console.log(part);
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substring(7, 13);
+// console.log(part);
 
+// Converting to Upper and Lower Case
+// A string is converted to upper case with toUpperCase():
 
+// A string is converted to lower case with toLowerCase():
+
+// let raihans = "raihan biswas";
+// let upper = raihans.toLocaleUpperCase();
+// console.log(upper);
+
+// let raihanc = "RAIHAN BISWAS";
+// let low = raihanc.toLowerCase();
+// console.log(low);
+
+// let text1 = "Hello";
+// let text2 = "World!";
+// let text3 = "How are you?";
+
+// text4 = text1.concat(" ", text2, " ", text3);
+// console.log(text4);
+
+// Replacing String Content
+// The replace() method replaces a specified value with another value in a string:
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("visit", "see");
+// console.log(newText);
+
+// ------------------JavaScript String split()-------------
