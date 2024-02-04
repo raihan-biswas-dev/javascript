911c3a185d7fcab04cc528858454b8e05ce273ca
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2024-02-04 14:02:11 +0600

    concat()

--- JavaScript/ArrayW3.js/arr.js
+++ JavaScript/ArrayW3.js/arr.js
@@ -1,5 +1,34 @@
-const points1 = new Array(40, 100, 1, 5, 25, 10);
-const points2 = [40, 100, 1, 5, 25, 10];
+// const points1 = new Array(40, 100, 1, 5, 25, 10);
+// const points2 = [40, 100, 1, 5, 25, 10];
 
-console.log(typeof points1);
-console.log(typeof points2);
+// console.log(points1);
+// console.log(points2);
+
+// const points1 = new Array(40, 100, 1);
+// const points2 = new Array(40, 100);
+// const points3 = new Array(40);
+// const points4 = [40];
+// console.log(points1);
+// console.log(points2);
+// console.log(points3);
+// console.log(points4);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.join(" + "));
+// console.log(fruits.join(" * "));
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+// console.log(myChildren);
+
+// let name = ["dola", "kushbu", 24, 3, 7, 8, 0, 1, 4, 5];
+// let co = ["poly", "joly", "abc", "bcd", "popy", "lipy"];
+
+// let younus = name.concat(co);
+// console.log(younus);
+// let st = younus.sort();
+// console.log(younus.reverse());
+
+
