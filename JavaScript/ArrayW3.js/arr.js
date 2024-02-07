21866a93a696082d1140bec8e9a43e81118f9065
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2024-02-07 12:26:32 +0600

    Array Search

--- JavaScript/ArrayW3.js/arr.js
+++ JavaScript/ArrayW3.js/arr.js
@@ -34,6 +34,76 @@
 // const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // document.write(fruits.copyWithin(2, 1));
 
-const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Pineapple"];
-fruits.copyWithin(2, 0, 2);
-console.log(fruits);
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi", "Pineapple"];
+// fruits.copyWithin(2, 0, 2);
+// console.log(fruits);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// let a = fruits.copyWithin(2, 0, 2);
+// console.log(a);
+
+/* 
+const myArr = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+];
+const newArr = myArr.flat();
+console.log(newArr);
+*/
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+// const fruits = ["Banana", "Orange", "Apple", "Kiwi", "Pineapple"];
+// fruits.splice(2, 3);
+// console.log(fruits);
+
+// const numbers = [4, 9, 16, 25, 29];
+// let first = numbers.find(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 26;
+// }
+
+// console.log(first);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.sort();
+// console.log(fruits);
+
+
+/*  */
+
+// ----------------Find() Method
+
+const numbers1 = [4, 9, 16, 25, 29, 18];
+let first1 = numbers1.find(myFunction);
+
+function myFunction1(value, index, array) {
+  return value > 25;
+}
+console.log(first1); // 29
+
+// ----------------findIndex() Method
+
+const numbers2 = [4, 9, 16, 25, 29];
+let first2 = numbers2.findIndex(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 5;
+}
+console.log(first2); // 1
+
+// ----------------- findLast() Method
+
+const temp1 = [27, 28, 30, 40, 42, 35, 30];
+let high = temp1.findLast((x) => x > 40);
+
+console.log(high); // 42
+
+// ---------------- findLastIndex() Method
+
+const temp2 = [27, 28, 30, 40, 42, 35, 30];
+let post = temp2.findLastIndex((x) => x > 40);
+
+console.log(post); // 4
